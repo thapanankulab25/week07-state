312a77254a7f996ad6230cb298036aab1a12abaf
nine009yu <152370454+nine009yu@users.noreply.github.com>
2024-01-19 14:25:02 +0700

    create func ref

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,36 +1,6 @@
-import React from 'react'
-import Button from './class-components'
-import { Calculator } from './calculator'
-import Calculator2 from './calculator';
-import EventData1 from './event-data'
-import { EventData2 } from './event-data'
-import { Table } from './event-data'
+import React from 'react';
+import RefsFunc from './refs-func';
 
-
-export default function App(){
-    return (
-        <>
-        
-        <br></br>
-        <center>Button</center>
-          <EventData2/>
-          <br></br>
-          <hr></hr>
-
-        <center >Calculator</center>
-          <Calculator2 />
-          <br></br>
-          <hr></hr>
-
-          <br></br>
-        <center>Table</center>
-          <Table />
-          <br />
-          <hr></hr>
-
-          <br/>
-
-        </>
-      );
+export default function App() {
+  return <RefsFunc />;
 }
-
