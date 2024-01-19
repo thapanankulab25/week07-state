699b5be47d71d6c777c55e551ae04abef0e5939b
nine009yu <152370454+nine009yu@users.noreply.github.com>
2024-01-19 14:53:56 +0700

    commit array-table

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RefsFunc from './refs-func';
+import RefsArray from './refs-array'
 
 export default function App() {
-  return <RefsFunc />;
+  return <RefsArray/>;
 }
